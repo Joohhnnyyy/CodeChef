19730c9bd1101f3b714eb4c03e39f0975c9e0b1d
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-06 00:28:44 +0530

    Added solution - LeetHub

---
+++ fsqrt/fsqrt.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int times;
+    cin >> times;
+    while(times--){
+        int a;
+        cin>> a;
+        cout << floor(sqrt(a)) << endl;
+    }
+}
