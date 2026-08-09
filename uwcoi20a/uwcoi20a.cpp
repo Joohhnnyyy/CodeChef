6d49daa87675bb232f7c04922a69d0c54d6baa0d
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-09 23:40:42 +0530

    Added solution - LeetHub

--- uwcoi20a/uwcoi20a.cpp
+++ uwcoi20a/uwcoi20a.cpp
@@ -5,7 +5,15 @@ int main() {
     // your code goes here
     int times;
     cin >> times;
-    while( times-- )
+    while( times-- )P{
 }
 
-times local
+partition snippet
+pop_heap snippet
+pqueue snippet
+prev_permutation snippet
+pri snippet
+pro snippet
+pub snippet
+push_heap snippet
+namespace local
