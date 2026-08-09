7dec5d65c8270526c54e555fb04615c9c0db0c1a
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-09 23:40:28 +0530

    Added solution - LeetHub

--- uwcoi20a/uwcoi20a.cpp
+++ uwcoi20a/uwcoi20a.cpp
@@ -1,24 +1,9 @@
-    int times ; 
-    cin >> times;
-    while(times--){
-        int n;
-        cin>> n;
-        vector<int> arr(n);
-        for(int i = 0 ; i < arr.size() ; i++ ){
-            cin >> arr[i];
-        }
-        int left = 0 , right = arr.size() - 1;
-        int peak = -1;
-        while( left <= right ){
-            int mid = left + ( right - left ) / 2;
-            if( arr[mid] < arr[mid + 1])left = mid + 1;
-            else {
-                peak = mid;
-                right = mid - 1;
-            }
-        }cout<< arr[peak] << endl;
-    }
-    
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+
 }
 
 int local
