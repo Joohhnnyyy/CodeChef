b8015af3136b6742b9cee6cd4098691e780c0dcb
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-09 23:42:29 +0530

    Added solution - LeetHub

--- uwcoi20a/uwcoi20a.cpp
+++ uwcoi20a/uwcoi20a.cpp
@@ -10,16 +10,14 @@ int main() {
         cin >> n;
         vector<int>arr(n);
         for(int i = 0 ; i < n ; i++)cin>> arr[i];
-        int 
+        int max
     }
 }
 
-int local
-int16_t keyword
-int32_t keyword
-int64_t keyword
-int8_t keyword
-uint16_t keyword
-uint32_t keyword
-uint64_t keyword
-uint8_t keyword
+max
+ 
+snippet
+max
+_element
+ 
+snippet
