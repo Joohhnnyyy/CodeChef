f1e94b1d936f9678d10149585eb57ef2f6c225e3
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-09 23:42:34 +0530

    Added solution - LeetHub

--- uwcoi20a/uwcoi20a.cpp
+++ uwcoi20a/uwcoi20a.cpp
@@ -10,7 +10,8 @@ int main() {
         cin >> n;
         vector<int>arr(n);
         for(int i = 0 ; i < n ; i++)cin>> arr[i];
-        int maximun = arr[0]
+        int maximun = arr[0];
+        
     }
 }
 
