8b9d35af3cf8b7c4222d9d6c50e41f0e5d209d13
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-09 23:43:13 +0530

    Added solution - LeetHub

--- uwcoi20a/uwcoi20a.cpp
+++ uwcoi20a/uwcoi20a.cpp
@@ -12,10 +12,50 @@ int main() {
         for(int i = 0 ; i < n ; i++)cin>> arr[i];
         int maximun = arr[0];
         for(int i = 0 ; i < arr.size() ; i++){
-            if(maximum < arr[i])maximum = 
+            if(maximum < arr[i])maximum = ar
         }
     }
 }
 
-maximum local
-maximun local
+ar
+r
+ 
+local
+ar
+ray
+ 
+snippet
+p
+ar
+tition
+ 
+snippet
+ch
+ar
+ 
+keyword
+ch
+ar
+16_t
+ 
+keyword
+ch
+ar
+32_t
+ 
+keyword
+bin
+ar
+y_search
+ 
+snippet
+wch
+ar
+_t
+ 
+keyword
+_Imagin
+ar
+y
+ 
+keyword
