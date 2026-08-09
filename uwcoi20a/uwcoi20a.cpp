62e0f368281f899175b448025088f98519793978
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-09 23:42:23 +0530

    Added solution - LeetHub

--- uwcoi20a/uwcoi20a.cpp
+++ uwcoi20a/uwcoi20a.cpp
@@ -9,49 +9,16 @@ int main() {
         int n ;
         cin >> n;
         vector<int>arr(n);
-        for(int i = 0 ; i < n ; i++)cin>> ar
+        for(int i = 0 ; i < n ; i++)cin>> arr[i];
     }
 }
 
-ar
-r
- 
-local
-ar
-ray
- 
-snippet
-p
-ar
-tition
- 
-snippet
-ch
-ar
- 
-keyword
-ch
-ar
-16_t
- 
-keyword
-ch
-ar
-32_t
- 
-keyword
-bin
-ar
-y_search
- 
-snippet
-wch
-ar
-_t
- 
-keyword
-_Imagin
-ar
-y
- 
-keyword
+int local
+include local
+if keyword
+inline keyword
+int keyword
+int16_t keyword
+int32_t keyword
+int64_t keyword
+int8_t keyword
