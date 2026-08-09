0fef1885c8098d3627b54cf5f08e23c790e30d8d
Joohhnnyyy <johnsonansh32@gmail.com>
2026-08-09 23:42:38 +0530

    Added solution - LeetHub

--- uwcoi20a/uwcoi20a.cpp
+++ uwcoi20a/uwcoi20a.cpp
@@ -11,12 +11,46 @@ int main() {
         vector<int>arr(n);
         for(int i = 0 ; i < n ; i++)cin>> arr[i];
         int maximun = arr[0];
-        for()
+        for(int i = 0 l)
     }
 }
 
-for local
-for_each snippet
-fore snippet
-fori snippet
-transform snippet
+l
+cm
+ 
+snippet
+l
+d
+ 
+snippet
+l
+ist
+ 
+snippet
+l
+ld
+ 
+snippet
+l
+ong
+ 
+keyword
+l
+ower_bound
+ 
+snippet
+whi
+l
+e
+ 
+local
+inc
+l
+ude
+ 
+local
+a
+l
+ignas
+ 
+keyword
