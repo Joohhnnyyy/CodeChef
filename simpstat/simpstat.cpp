30ff0898e93d03cceba5d7d8dc8f5f9bf466cc72
Joohhnnyyy <johnsonansh32@gmail.com>
2026-09-10 23:10:46 +0530

    Added solution - LeetHub

--- simpstat/simpstat.cpp
+++ simpstat/simpstat.cpp
@@ -1,1 +1,28 @@
-# cook your dish here
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        // your code goes here
+        sort(a.begin() , a.end());
+        int sum = 0;
+        for(int i = 1 ; i < a.size() - 2 ; i++){
+            sum += a[i];
+        }
+        cout << sum / (arr.size() - 3) << endl;
+    }
+
+}
+
+end local
+friend keyword
+lower_bound snippet
+upper_bound snippet
