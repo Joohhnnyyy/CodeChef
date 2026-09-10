9ee87a1b148c9c698e3b86fe6fc3313c1d128182
Joohhnnyyy <johnsonansh32@gmail.com>
2026-09-10 23:12:44 +0530

    Added solution - LeetHub

--- simpstat/simpstat.cpp
+++ simpstat/simpstat.cpp
@@ -17,17 +17,13 @@ int main() {
         for(int i = k ; i < n - k ; i++){
             sum += a[i];
         }
-        cout << fixed << setprecision(<<sum / (n - 2 * k ) << endl;
+        cout << fixed << setprecision(6)<<sum / (n - 2 * k ) << endl;
     }
 
 }
 
-set snippet
-set_difference snippet
-set_intersection snippet
-set_union snippet
-mset snippet
-uset snippet
-umset snippet
-size_t keyword
-stable_partition snippet
+int64_t keyword
+int16_t keyword
+uint64_t keyword
+char16_t keyword
+uint16_t keyword
