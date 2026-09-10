b46b6ae4a5cac77d06be0e5398ba1f32caaf9181
Joohhnnyyy <johnsonansh32@gmail.com>
2026-09-10 23:12:40 +0530

    Added solution - LeetHub

--- simpstat/simpstat.cpp
+++ simpstat/simpstat.cpp
@@ -17,7 +17,7 @@ int main() {
         for(int i = k ; i < n - k ; i++){
             sum += a[i];
         }
-        cout << fixed << setp<<sum / (n - 2 * k ) << endl;
+        cout << fixed << setpre<<sum / (n - 2 * k ) << endl;
     }
 
 }
