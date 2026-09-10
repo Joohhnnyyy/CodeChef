8475307663c0b23bb841dae5a08d0874b96aa1a7
Joohhnnyyy <johnsonansh32@gmail.com>
2026-09-10 22:55:54 +0530

    Added solution - LeetHub

---
+++ tsort/tsort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int times;
+    cin >> times;
+    while(times--){
+        int n ;
+        cin >> n;
+        
+    }
+}
+
+int local
+include local
+if keyword
+inline keyword
+int keyword
+int16_t keyword
+int32_t keyword
+int64_t keyword
+int8_t keyword
